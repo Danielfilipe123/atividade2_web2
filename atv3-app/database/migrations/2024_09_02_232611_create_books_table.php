127b32dd6d23f56ef2a90f44622f5eb9ccf79ac6
Danielfilipe123 <dfsa1@discente.ifpe.edu.br>
2024-09-02 21:53:31 -0300

    atividade3_Sedeers

--- atv3-app/database/migrations/2024_09_02_232611_create_books_table.php
+++ atv3-app/database/migrations/2024_09_02_232611_create_books_table.php
@@ -9,19 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->unsignedBigInteger('author_id');
             $table->unsignedBigInteger('category_id');
+            $table->integer('published_year')->nullable();
             $table->timestamps();
-    
+
             $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
         });
     }
+
     /**
      * Reverse the migrations.
      */
